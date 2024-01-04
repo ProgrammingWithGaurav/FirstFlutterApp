4e16ac1ecb274e7c4d9c3cc7618ac1110c794c93
ProgrammingWithGaurav <gaurav299kumar@gmail.com>
2024-01-04 16:22:09 +0530

    Login Page Done ✅

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/login.dart';
+import 'post.dart';
+import 'post-list.dart';
+import 'text-input.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class Post {
-  String body;
-  String author;
-  int likes = 0;
-  bool userLiked = false;
-
-  Post(this.body, this.author);
-
-  void likePost() {
-    this.userLiked = !this.userLiked;
-    if (this.userLiked) {
-      this.likes += 1;
-    } else {
-      this.likes -= 1;
-    }
-  }
 }
 
 class MyApp extends StatelessWidget {
@@ -34,125 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<Post> posts = [];
-
-  void newPost(String text) {
-    this.setState(() {
-      posts.add(new Post(text, "User1"));
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text("Hello World")),
-        body: Column(children: <Widget>[
-          Expanded(child: PostList(this.posts)),
-          TextInputWidget(this.newPost),
-        ]));
-  }
-}
-
-class TextInputWidget extends StatefulWidget {
-  final Function(String) callback;
-
-  TextInputWidget(this.callback);
-
-  @override
-  State<TextInputWidget> createState() => _TextInputWidgetState();
-}
-
-class _TextInputWidgetState extends State<TextInputWidget> {
-  final controller = TextEditingController();
-
-  @override
-  void dispose() {
-    // refreshing the controller when a state is change
-    super.dispose();
-    controller.dispose();
-  }
-
-  @override
-  void click() {
-    FocusScope.of(context).unfocus();
-    widget.callback(controller.text);
-    controller.clear();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-        controller: this.controller,
-        decoration: InputDecoration(
-          prefixIcon: Icon(Icons.message),
-          labelText: "Type a message",
-          suffixIcon: IconButton(
-            icon: Icon(Icons.send),
-            splashColor: Colors.deepPurple,
-            tooltip: "Post Message",
-            onPressed: this.click,
-          ),
-        ));
-  }
-}
-
-class PostList extends StatefulWidget {
-  final List<Post> listItems;
-
-  PostList(this.listItems);
-
-  @override
-  State<PostList> createState() => _PostListState();
-}
-
-class _PostListState extends State<PostList> {
-  void like(Function callBack) {
-    this.setState(() {
-      callBack();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: this.widget.listItems.length,
-      itemBuilder: (context, index) {
-        var post = this.widget.listItems[index];
-        return Card(
-            child: Row(children: <Widget>[
-          Expanded(
-              child: ListTile(
-            title: Text(post.body),
-            subtitle: Text(post.author),
-          )),
-          Row(children: <Widget>[
-            Container(
-              child:
-                  Text(post.likes.toString(), style: TextStyle(fontSize: 20)),
-              padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
-            ),
-            IconButton(
-                icon: Icon(
-                  Icons.thumb_up,
-                  color: post.userLiked ? Colors.deepOrange[400] : Colors.grey,
-                ),
-                onPressed: () => this.like(post.likePost)),
-          ])
-        ]));
-      },
+      home: LoginPage(),
     );
   }
 }
---
+++ lib/text-input.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class TextInputWidget extends StatefulWidget {
+  final Function(String) callback;
+
+  TextInputWidget(this.callback);
+
+  @override
+  State<TextInputWidget> createState() => _TextInputWidgetState();
+}
+
+class _TextInputWidgetState extends State<TextInputWidget> {
+  final controller = TextEditingController();
+
+  @override
+  void dispose() {
+    // refreshing the controller when a state is change
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  void click() {
+    widget.callback(controller.text);
+    FocusScope.of(context).unfocus();
+    controller.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+        controller: this.controller,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.message),
+          labelText: "Type a message",
+          suffixIcon: IconButton(
+            icon: Icon(Icons.send),
+            splashColor: Colors.deepPurple,
+            tooltip: "Post Message",
+            onPressed: this.click,
+          ),
+        ));
+  }
+}
